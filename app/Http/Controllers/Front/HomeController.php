b57650a9ebd542ed90058c28b40e4751fb3480a7
AzliaAfifah <azlianisrinaafifah9706@gmail.com>
2024-09-03 22:10:13 +0700

    berubah

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\HomePageItem;
 
 class HomeController extends Controller
 {
@@ -15,5 +16,11 @@ class HomeController extends Controller
     public function karya() 
     {
         return view('front.karya');
+    }
+
+    public function contact() 
+    {
+        $page_data = HomePageItem::where('id',1)->first();
+        return view('front.home', compact('page_data'));
     }
 }
