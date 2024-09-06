a0ba65d64d085b77699efce1e6f2effd713017d1
AzliaAfifah <azlianisrinaafifah9706@gmail.com>
2024-09-06 14:27:13 +0700

    ubah

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -70,8 +70,7 @@
 
       <div class="container">
         <div class="row justify-content-center" data-aos="zoom-out">
-          <div class="col-xl-7 col-lg-9 text-center">   
-              {{-- bagian heading --}}
+          <div class="col-xl-7 col-lg-9 text-center"> 
             <h1>WELCOME TO WJLRC WEBSITE</h1>
             <p>Tim kami di WJLRC berkomitmen untuk mencapai keunggulan dalam semua kegiatan ekstrakurikuler kami.</p>
           </div>
