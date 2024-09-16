1c5b5c8733fbfba83f1b9275e1ad592003c3c5fc
AzliaAfifah <azlianisrinaafifah9706@gmail.com>
2024-09-16 14:00:29 +0700

    aku ajah

--- resources/views/layout/scripts.blade.php
+++ resources/views/layout/scripts.blade.php
@@ -1,6 +1,6 @@
 <!-- Vendor JS Files -->
 <script src="{{ asset('dist_front/landing_page/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('dist_front/landing_page/vendor/php-email-form/validate.js') }}"></script>
+  {{-- <script src="{{ asset('dist_front/landing_page/vendor/php-email-form/validate.js') }}"></script> --}}
   <script src="{{ asset('dist_front/landing_page/vendor/aos/aos.js') }}"></script>
   <script src="{{ asset('dist_front/landing_page/vendor/purecounter/purecounter_vanilla.js') }}"></script>
   <script src="{{ asset('dist_front/landing_page/vendor/glightbox/js/glightbox.min.js') }}"></script>
