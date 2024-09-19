47d4868939b18d13200e49e8592f47a00cbf2197
byndewan <abyandev657@gmail.com>
2024-09-19 20:29:59 +0700

    ubah

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Karya;
 use App\Models\Kontak;
 use App\Models\Tim;
+use Auth;
 use Illuminate\Http\Request;
 use App\Models\HomePageItem;
 use App\Models\Comment;
@@ -15,19 +16,16 @@ class HomeController extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
         $page_data = Tim::where( 'id','3')->first();
         $data_kontak = Kontak::where('id','1')->first();
         $tim_data = Tim::orderBy('id','asc')->get();
         $karya_data = Karya::orderBy('id','asc')->limit(6)->get();
         return view('front.home',compact('karya_data','page_data','tim_data'))->with('data_kontak', $data_kontak);
-=======
         $data_page = HomePageItem::where('id',1)->first();
         $page_data = Tim::where('id','1')->first();
         $tim_data = Tim::orderBy('id','asc')->get();
         $karya_data = Karya::orderBy('id','asc')->get();
         return view('front.home',compact('karya_data','page_data','data_page','tim_data'));
->>>>>>> f021373848b96d41fa8804e99dc16e1c96465377
     }
 
     public function karya()
@@ -38,11 +36,9 @@ class HomeController extends Controller
 
     public function detail_karya($id)
     {
-<<<<<<< HEAD
         $karya_data = Karya::orderBy('id','asc')->get();
         $data_karya = Karya::where('id',$id)->first();
         return view('detail_halaman.detail_karya', compact('karya_data'))->with('data_karya', $data_karya);
-=======
         $comments = Comment::with('replies.user', 'user')->get();
         return view('detail_halaman.detail_karya', compact('comments'));
     }
@@ -67,16 +63,15 @@ class HomeController extends Controller
         $request->validate([
             'body' => 'required'
         ]);
-    
+
         Reply::create([
             'comment_id' => $comment_id,
             'user_id' => Auth::id(),
             'body' => $request->input('body'),
             'created_at' => now(),
         ]);
-    
+
         return redirect()->back();
->>>>>>> f021373848b96d41fa8804e99dc16e1c96465377
     }
 
     public function contact()
